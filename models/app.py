25ac633c905c9743d8f209f3dee2b0cc70dd6ef0
Wisieneg <wojtas.w1@op.pl>
2021-04-16 11:00:41 +0200

    aa

---
+++ models/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, redirect, url_for, request, session, g
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_sqlalchemy import SQLAlchemy
+from cas import CASClient
+from models import *
+import sqlite3
+import sys
+
+app = Flask(__name__)
+
+app.secret_key = 'sTzMzxFX8BcJt3wuvNvDeQ'
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///master.db'
+
+DATABASE = SQLAlchemy(app)
+admin = Admin(app, name='Ankieter', template_mode='bootstrap3')
+admin.add_view(ModelView(User, DATABASE.session))
+#DATABASE = './master.db'
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        username = session['username']
+        return '''<p>Witaj {}</p>'''.format(username)
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['uname']
+        return redirect(url_for('index'))
+    return '''
+    <form action="{}" method="post">
+     <div>
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required></br>
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+      <br><button type="submit">Login</button>
+     </div>
+    </form>'''.format(url_for('login'))
+
+
+if __name__ == '__main__':
+
+    app.run()
---
+++ models/models.py
@@ -0,0 +1,68 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///master.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "Users"
+    id = db.Column(db.Integer, primary_key=True)
+    CasLogin = db.Column(db.String(80), unique=True, nullable=False)
+    Role = db.Column(db.Integer, default=2, nullable=False)
+
+
+class Group(db.Model):
+    __tablename__ = "Groups"
+    id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(25), nullable=False)
+
+
+class Survey(db.Model):
+    __tablename__ = "Surveys"
+    id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(80), nullable=False)
+    AnkieterId = db.Column(db.Integer, unique=True)
+
+
+class Report(db.Model):
+    __tablename__ = "Reports"
+    id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(80), nullable=False)
+    SurveyId = db.Column(db.Integer, db.ForeignKey('Surveys.id'), nullable=False)
+
+
+class UserGroup(db.Model):
+    __tablename__ = "UserGroups"
+    GroupId = db.Column(db.Integer, db.ForeignKey('Groups.id'), primary_key=True)
+    UserId = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True)
+
+
+class SurveyGroup(db.Model):
+    __tablename__ = "SurveyGroups"
+    SurveyId = db.Column(db.Integer, db.ForeignKey('Surveys.id'), primary_key=True)
+    GroupId = db.Column(db.Integer, db.ForeignKey('Groups.id'), primary_key=True)
+
+
+class ReportGroup(db.Model):
+    __tablename__ = "ReportGroups"
+    ReportId = db.Column(db.Integer, db.ForeignKey('Reports.id'), primary_key=True)
+    GroupId = db.Column(db.Integer, db.ForeignKey('Groups.id'), primary_key=True)
+
+
+class SurveyPermission(db.Model):
+    __tablename__ = "SurveyPermissions"
+    SurveyId = db.Column(db.Integer, db.ForeignKey('Surveys.id'), primary_key=True)
+    UserId = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True)
+    Type = db.Column(db.Integer, default=2, nullable=False)
+
+
+class ReportPermission(db.Model):
+    __tablename__ = "ReportPermissions"
+    ReportId = db.Column(db.Integer, db.ForeignKey('Reports.id'), primary_key=True)
+    UserId = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True)
+    Type = db.Column(db.Integer, default=2, nullable=False)
+
+if __name__ == "__main__":
+    db.create_all()
