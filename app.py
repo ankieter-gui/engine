89b7c53d057d6a33fb7b55a5066a5f658d4abf76
Wisieneg <wojtas.w1@op.pl>
2021-05-05 01:55:31 +0200

    Added mock api response

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from flask import redirect, url_for, request, session, g
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
+from flask_cors import CORS
+from flask_jsonpify import jsonify
 from cas import CASClient
 from setup import *
 import sqlite3
-import sys
+
 
 app = Flask(__name__)
+api = Api(app)
+CORS(app)
 
 app.config.from_mapping(
     SECRET_KEY='sTzMzxFX8BcJt3wuvNvDeQ',
@@ -29,11 +34,26 @@ cas_client = CASClient(
 
 # DATABASE = './master.db'
 
+class Dashboard(Resource):
+    def get(self):
+        user = User.query.filter_by(CasLogin=session['username']).first()
+        surveys = SurveyPermission.query.filter_by(UserId=user.id).all()
+        result = {}
+        for s in surveys:
+            result[s.SurveyId] = {
+                'survey_id': s.SurveyId,
+                'user_id': s.UserId
+            }
+        return jsonify(result)
+
+
+api.add_resource(Dashboard,'/dashboard')
+
 @app.route('/')
 def index():
     if 'username' in session:
         username = session['username']
-        return '''<p>Witaj {}</p>'''.format(username)
+        return '''<p>Witaj {}</p></br><a href="{}">Wyloguj</a>'''.format(username,url_for('logout'))
     return redirect(url_for('login'))
 
 
@@ -51,6 +71,11 @@ def login():
     session['username'] = user
     return redirect(url_for('index'))
 
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(cas_client.get_logout_url())
 
 if __name__ == '__main__':
     app.run()
