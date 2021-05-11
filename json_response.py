70d5bfdaee20d990f3905994d1f615bb9024a45d
Rafał <rafcikp2@gmail.com>
2021-05-11 18:30:50 +0200

    rename variable

--- json_response.py
+++ json_response.py
@@ -5,13 +5,13 @@ class JsonResponse:
     CODE_400 = '400'
     CODE_200 = '200'
 
-    def __init__(self, error_message: str) -> None:
-        self.error_message = error_message
+    def __init__(self, message: str) -> None:
+        self.message = message
 
     def get_json_error_response(self):
-        response = {"status_code": self.CODE_400, 'error_message': self.error_message}
+        response = {"status_code": self.CODE_400, 'error_message': self.message}
         return jsonify(response)
 
     def get_json_success_response(self):
-        response = {"status_code": self.CODE_200, 'success_message': self.error_message}
+        response = {"status_code": self.CODE_200, 'success_message': self.message}
         return jsonify(response)
