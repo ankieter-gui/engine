16f5f60f89731e135b2ac23134aa3b0dd0311f25
Rafał <rafcikp2@gmail.com>
2021-05-11 19:53:54 +0200

    add get_data

--- json_response.py
+++ json_response.py
@@ -5,13 +5,10 @@ class JsonResponse:
     CODE_400 = '400'
     CODE_200 = '200'
 
-    def __init__(self, message: str) -> None:
-        self.message = message
-
-    def get_json_error_response(self):
-        response = {"status_code": self.CODE_400, 'error_message': self.message}
+    def get_json_error_response(self, message: str):
+        response = {"status_code": self.CODE_400, "error_message": message}
         return jsonify(response)
 
-    def get_json_success_response(self):
-        response = {"status_code": self.CODE_200, 'success_message': self.message}
+    def get_json_success_response(self, message: str, data):
+        response = {"status_code": self.CODE_200, "success_message": message, "data": data}
         return jsonify(response)
--- request_survey.py
+++ request_survey.py
@@ -1,4 +1,6 @@
 from json_response import JsonResponse
+import sqlite3
+import pandas
 
 
 class RequestSurvey:
@@ -8,7 +10,29 @@ class RequestSurvey:
         self.json_data = json_data
 
     def execute_request(self):
+        response = JsonResponse()
         if self.SURVEY_ID not in self.json_data:
-            return JsonResponse(f'Parameter {self.SURVEY_ID} not found').get_json_error_response()
+            return response.get_json_error_response(f'Parameter {self.SURVEY_ID} not found')
+        if len(self.json_data["get"][0]) != len(self.json_data["as"]):
+            return response.get_json_error_response('Wrong amount of given "get" and "as" values')
 
-        return JsonResponse('All done').get_json_success_response()
+        data = self.get_data()
+        return data
+        # return response.get_json_success_response('Success', data)
+
+    def get_data(self):
+        conn = sqlite3.connect("survey_data/" + str(self.json_data[self.SURVEY_ID]) + '.db')
+        df = pandas.read_sql_query('''
+        SELECT stopien_stodiow, avg(ocena), count(ocena)
+        FROM data
+        GROUP BY stopien_stodiow
+        ''', conn)
+        # cur = conn.cursor()
+        # cur.execute('''
+        # SELECT stopien_stodiow, avg(ocena), count(ocena)
+        # FROM data
+        # GROUP BY stopien_stodiow
+        # ''')
+        # data = cur.fetchall()
+        conn.close()
+        return df.to_json()
