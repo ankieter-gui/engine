9f212b268b7fbc5ddbee6cfd7f0127e3dafbcb0f
kiryk <alkiryk@wp.pl>
2021-05-13 23:18:24 +0200

    api classes removed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import redirect, url_for, request, session, g
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
+from flask_ADMIN import Admin
+from flask_ADMIN.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
 from flask_cors import CORS
@@ -25,68 +25,59 @@ app.config.from_mapping(
 )
 
 DATABASE = SQLAlchemy(app)
-admin = Admin(app, name='Ankieter', template_mode='bootstrap3')
-admin.add_view(ModelView(User, DATABASE.session))
-cas_url = 'https://cas.amu.edu.pl/cas'
+ADMIN = Admin(app, name='Ankieter', template_mode='bootstrap3')
+ADMIN.add_view(ModelView(User, DATABASE.session))
 
-cas_client = CASClient(
+CAS_CLIENT = CASClient(
     version=2,
     service_url='http://localhost:5000/login',
     server_url='https://cas.amu.edu.pl/cas/'
 )
 
 
-# DATABASE = './master.db'
-
-
-class Dashboard(Resource):
-    def get(self):
-        user = User.query.filter_by(CasLogin=session['username']).first()
-        survey_permissions = SurveyPermission.query.filter_by(UserId=user.id).all()
-        result = {}
-        for sp in survey_permissions:
-            survey = Survey.query.filter_by(id=sp.SurveyId).first()
-            meta = self.get_meta(survey.AnkieterId)
-            print(meta)
-            #na szybko - do poprawy
-            result[survey.AnkieterId] = {
-                'survey_id': survey.AnkieterId,
-                'user_id': sp.UserId,
-                'startedOn': meta[0][0],
-                'endsOn': meta[0][1],
-                'isActive': meta[0][2],
-                'QuestionsAmount': meta[0][3]
-            }
-        return jsonify(result)
-
-    def get_meta(self, survey_id):
+@app.route('/dashboard', methods=['GET'])
+def get_dashboard():
+    def get_meta(survey_id):
         conn = sqlite3.connect("data/" + str(survey_id) + '.db')
         cur = conn.cursor()
         cur.execute("select * from meta")
         data = cur.fetchall()
         conn.close()
         return data
+    user = User.query.filter_by(CasLogin=session['username']).first()
+    survey_permissions = SurveyPermission.query.filter_by(UserId=user.id).all()
+    result = {}
+    for sp in survey_permissions:
+        survey = Survey.query.filter_by(id=sp.SurveyId).first()
+        meta = get_meta(survey.AnkieterId)
+        print(meta)
+        #na szybko - do poprawy
+        result[survey.AnkieterId] = {
+            'survey_id': survey.AnkieterId,
+            'user_id': sp.UserId,
+            'startedOn': meta[0][0],
+            'endsOn': meta[0][1],
+            'isActive': meta[0][2],
+            'QuestionsAmount': meta[0][3]
+        }
+    return jsonify(result)
 
 
-class RequestSurvey(Resource):
-    def post(self, survey_id):
-        if not request.json:
-            # TODO: return json with errors
-            return
-        json_request = request.json
-        script_absolute_directory_path = os.path.dirname(os.path.realpath(__file__))
-        db_absolute_path=path.join(script_absolute_directory_path,"data", survey_id+".db")
-        conn = sqlite3.connect(db_absolute_path)
-        result = request_survey(json_request, conn)
-        conn.close()
-        return result
+@app.route('/data/<int:survey_id>', methods=['POST'])
+def get_data(survey_id):
+    if not request.json:
+        # TODO: return json with errors
+        return
+    json_request = request.json
+    script_absolute_directory_path = os.path.dirname(os.path.realpath(__file__))
+    db_absolute_path=path.join(script_absolute_directory_path,"data", survey_id+".db")
+    conn = sqlite3.connect(db_absolute_path)
+    result = request_survey(json_request, conn)
+    conn.close()
+    return result
 
 
-api.add_resource(Dashboard, '/dashboard')
-api.add_resource(RequestSurvey, '/data/<survey_id>')
-
-
-@app.route('/data/types/<survey_id>', methods=['GET'])
+@app.route('/data/types/<int:survey_id>', methods=['GET'])
 def data_types(survey_id):
     conn = sqlite3.connect(f'data/{survey_id}.db')
     types = get_column_types(conn)
@@ -106,9 +97,9 @@ def index():
 def login():
     ticket = request.args.get('ticket')
     if not ticket:
-        return redirect(cas_client.get_login_url())
+        return redirect(CAS_CLIENT.get_login_url())
 
-    user, attributes, pgtiou = cas_client.verify_ticket(ticket)
+    user, attributes, pgtiou = CAS_CLIENT.verify_ticket(ticket)
 
     if not user:
         return '<p>Failed to verify</p>'
@@ -120,7 +111,7 @@ def login():
 @app.route('/logout')
 def logout():
     session.clear()
-    return redirect(cas_client.get_logout_url())
+    return redirect(CAS_CLIENT.get_logout_url())
 
 
 if __name__ == '__main__':
