900f6850466d84684492d45f69e5c9b62c99ed69
Rafał <rafcikp2@gmail.com>
2021-05-19 16:40:19 +0200

    fix unique constraint error(sometimes occur)

--- setup.py
+++ setup.py
@@ -169,7 +169,7 @@ if __name__ == "__main__":
     for primary_key in report_group:
         db.session.add(ReportGroup(ReportId=primary_key[0], GroupId=primary_key[1]))
 
-    survey_permission = [(random.randint(1, surveys_amount), random.randint(1, USERS_AMOUNT)) for i in range(2)]
+    survey_permission = [(random.randint(1, surveys_amount), random.randint(2, USERS_AMOUNT)) for i in range(20)]
     survey_permission = remove_duplicates(survey_permission)
     for primary_key in survey_permission:
         db.session.add(SurveyPermission(SurveyId=primary_key[0], UserId=primary_key[1], Type=random.randint(0, 2)))
