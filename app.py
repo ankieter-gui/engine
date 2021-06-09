66f159ac34edebe3b7ae3e649705a51c9e1e5907
Wisieneg <wojtas.w1@op.pl>
2021-06-10 00:16:25 +0200

    minor fix

--- app.py
+++ app.py
@@ -357,8 +357,8 @@ def get_groups():
 @on_errors('could not add users to groups')
 @for_roles('s')
 def set_group():
-    for group, ids in request.json:
-        grammar.check([int], ids)
+    for group, *ids in request.json:
+        #grammar.check([int], ids)
         for id in ids:
             user = database.get_user(id)
             database.set_user_group(user, group)
@@ -373,7 +373,7 @@ def set_group():
 @for_roles('s')
 def unset_group():
     for group, *ids in request.json:
-        grammar.check([int], ids)
+        #grammar.check([int], ids)
         for id in ids:
             user = database.get_user(id)
             database.unset_user_group(user, group)
