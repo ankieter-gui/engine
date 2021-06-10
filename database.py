e8e2fa36cc1326a24a73f3b966c73d8fed5a9a99
kiryk <alkiryk@wp.pl>
2021-06-10 11:44:31 +0200

    fixes in salt generation

--- database.py
+++ database.py
@@ -140,7 +140,7 @@ def get_permission_link(permission: Permission, object: Literal['s', 'r'], objec
         return link.Salt + str(link.id)
     salt = secrets.randbits(5*SALT_LENGTH)
     salt = salt.to_bytes(5*SALT_LENGTH//8+1, byteorder='big')
-    salt = str(b32encode(salt).decode('utf-8'))
+    salt = str(b32encode(salt))
     link = Link(
         Salt=salt,
         Type=permission,
