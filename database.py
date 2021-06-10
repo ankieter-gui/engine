166e8cdccfa625e9da1ed7acb35aa5bd5157e990
kiryk <alkiryk@wp.pl>
2021-06-10 12:25:05 +0200

    lowercase salt in sharing links

--- database.py
+++ database.py
@@ -162,6 +162,7 @@ def get_permission_link(permission: Permission, object: Literal['s', 'r'], objec
     salt = secrets.randbits(5*SALT_LENGTH)
     salt = salt.to_bytes(5*SALT_LENGTH//8+1, byteorder='big')
     salt = b32encode(salt).decode('utf-8')[SALT_LENGTH:]
+    salt = salt.lower()
     link = Link(
         Salt=salt,
         Type=permission,
