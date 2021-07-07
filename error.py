43e13e5841d96f5186e7efb3344c15fb7d6bc501
Rafał <rafcikp2@gmail.com>
2021-07-07 08:51:13 +0200

    error.py docstrings

--- error.py
+++ error.py
@@ -1,11 +1,27 @@
 class Generic(Exception):
-    def __init__(self, message):
+    def __init__(self, message: str):
         self.message = message
 
-    def add_details(self, message):
+    def add_details(self, message: str):
+        """Add details to the error message.
+
+        Keyword arguments:
+        message -- details to be concatenated
+
+        Return value:
+        returns self
+        """
+
         self.message = f'{message}: {self.message}'
         return self
 
+
 class API(Generic):
-    def as_dict(self):
+    def as_dict(self) -> dict:
+        """Return message as a dict.
+
+        Return value:
+        returns dict object
+        """
+
         return {'error': self.message}
