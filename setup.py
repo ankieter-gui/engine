e9a3f7f804ea2bc645c953de6b9df57b06a0d601
kiryk <alkiryk@wp.pl>
2021-08-29 14:56:06 +0200

    simplify setup

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python
+
 from datetime import datetime
 from itertools import product
 from functools import reduce
 from typing import List, Dict
 from database import *
-import faker
 import random
 import string
 import os
@@ -42,32 +43,25 @@ def get_sample_tuples(n: int, *args: int) -> List[tuple]:
 
 
 if __name__ == "__main__":
+    db.drop_all()
+    db.create_all()
+
     for dir in ['data', 'raw', 'report', 'bkg']:
         if not os.path.exists(dir):
             os.makedirs(dir)
 
-    fake = faker.Faker(locale="pl_PL")
-
-    USERS_AMOUNT = 20
-
-
-    GROUPS = ['dziekani', 'wmi', 'wb', 'wpik']
-    GROUPS_AMOUNT = len(GROUPS)
+    bkgs = os.listdir('bkg')
 
     surveys_amount = 0
-
-    db.drop_all()
-    db.create_all()
-
-    bkgs = os.listdir(path.join(ABSOLUTE_DIR_PATH,'bkg'))
 
     for filename in os.listdir('raw'):
         if filename.endswith(".csv"):
             survey = Survey(
-                Name='ankieta testowa' + str(random.randint(0, 99999)),
-                StartedOn=datetime(2020, 3, random.randint(1, 31)),
-                EndsOn=datetime(2021, 6, random.randint(1, 30)),
-                IsActive=random.randint(0, 1),
+                Name=f'ankieta z {filename}',
+
+                StartedOn=datetime.now(),
+                EndsOn=datetime.now() + datetime.timedelta(days=56),
+                IsActive=random.randint(1),
                 BackgroundImg=random.choice(bkgs))
             db.session.add(survey)
             db.session.commit()
@@ -76,33 +70,15 @@ if __name__ == "__main__":
             csv_to_db(survey, filename)
             surveys_amount += 1
 
-    emails = [fake.unique.email() for x in range(USERS_AMOUNT)]
-    for _ in range(USERS_AMOUNT - 1):
-        cas_login = emails[_]
-        pesel = ''.join([random.choice(string.digits) for i in range(11)])
-        # role = random.randint(0, 2)
-        role = random.choice('gus')
-        db.session.add(User(CasLogin=cas_login,Pesel=pesel, Role=role, FetchData=False))
+    # Create the administrator
+    db.session.add(User(CasLogin='admin',
+                        Pesel='9999999999',
+                        Role='s',
+                        FetchData=False))
 
-    for g_id, u_id in get_sample_tuples(18, GROUPS_AMOUNT, USERS_AMOUNT):
-        db.session.add(UserGroup(Group=GROUPS[g_id-1], UserId=u_id))
-
-    for s_id, g_id in get_sample_tuples(18, surveys_amount, GROUPS_AMOUNT):
-        db.session.add(SurveyGroup(SurveyId=s_id, Group=GROUPS[g_id-1]))
-
-    for s_id, u_id in get_sample_tuples(18, surveys_amount, USERS_AMOUNT):
-        db.session.add(SurveyPermission(SurveyId=s_id, UserId=u_id, Type=random.choice('rwo')))
-
-    email = input('Enter your email\n')
-    user = User.query.filter_by(id=1).first()
-    user.CasLogin = email
-    user.Role = 's'
-
-    for survey in Survey.query.all():
-        set_survey_permission(survey, user, 'o')
-
-    user = User.query.filter_by(id=2).first()
-    user.CasLogin = GUEST_NAME
-    user.Role = 'g'
+    db.session.add(User(CasLogin=GUEST_NAME,
+                        Pesel='9999999998',
+                        Role='g',
+                        FetchData=True))
 
     db.session.commit()
