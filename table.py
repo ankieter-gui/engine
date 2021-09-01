535549102ff95c2a61c87a424ee5bb1ed376704d
kiryk <alkiryk@wp.pl>
2021-09-01 16:01:36 +0200

    hotfix

--- table.py
+++ table.py
@@ -176,7 +176,7 @@ def aggregate(json_query, data):
 
             columns[column].append(aggr.func)
 
-    if 'by' not in json_query:
+    if 'by' not in json_query or not json_query['by']:
         json_query['by'] = ['*']
 
     result = None
