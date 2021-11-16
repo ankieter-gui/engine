f98e387d781992f7866cf07ef2ce9e73fdf070cb
kiryk <alkiryk@wp.pl>
2021-11-16 23:57:51 +0100

    make setup.py more verbose

--- setup.py
+++ setup.py
@@ -9,38 +9,29 @@ import random
 import os
 
 
-def get_survey_quest_num(survey: Survey) -> int:
-    """Get number of questions for given survey
-
-    :param survey: survey
-    :type survey: object: Survey
-    :return: number of columns
-    :rtype: int
-    """
-
-    conn = open_survey(survey)
-    num = len(get_columns(conn))
-    conn.close()
-    return num
-
-
-def get_sample_tuples(n: int, *args: int) -> List[tuple]:
-    """Generate sample tuples.
-
-    Keyword arguments:
-    n -- number of rows to generate
-
-    Return value:
-    returns list of generated values
-    """
-
-    n = min(n, reduce(lambda a, b: a*b, args))
-    args = map(lambda x: range(1, x+1), args)
-    s = random.sample(sorted(product(*args)), n)
-    return s
+def ask(question, choices):
+    while True:
+        print(f'{question} ({", ".join([str(c) for c in choices])}) [{choices[0] if choices else ""}]', end=' ')
+        choice = input().strip()
+        if len(choice) == 0:
+            if choices:
+                choice = choices[0]
+            else:
+                continue
+        if choices:
+            try:
+                if type(choices[0]) is int:
+                    choice = int(choice)
+                elif type(choices[0]) is float:
+                    chocie = float(choice)
+            except:
+                continue
+        if choice in choices or len(choices) <= 1:
+            break
+    return choice
 
 
-def setup():
+def setup(admin, pesel):
     db.drop_all()
     db.create_all()
 
@@ -50,33 +41,42 @@ def setup():
 
     backgrounds = os.listdir('bkg')
 
-    for filename in os.listdir('raw'):
-        if filename.endswith(".csv"):
-            survey = Survey(
-                Name=f'ankieta z {filename}',
-                StartedOn=datetime.now(),
-                EndsOn=datetime.now() + timedelta(days=56),
-                IsActive=random.randint(0, 1),
-                BackgroundImg=random.choice(backgrounds))
-            db.session.add(survey)
-            db.session.commit()
-            csv_to_db(survey, filename)
-            survey.QuestionCount = get_survey_quest_num(survey)
-            db.session.commit()
-
-    db.session.add(User(CasLogin='admin',
-                        Pesel='9999999999',
+    db.session.add(User(CasLogin=admin,
+                        Pesel=pesel,
                         Role='s',
                         FetchData=False))
 
     db.session.add(User(CasLogin=GUEST_NAME,
-                        Pesel='9999999998',
+                        Pesel='99999999998',
                         Role='g',
                         FetchData=True))
-
     db.session.commit()
 
-
 if __name__ == "__main__":
-    setup()
+    if not os.path.exists('config.py') or 't' == ask('Konfiguracja już istnieje, czy zamierzasz utworzyć ją na nowo?', ['n', 't']):
+        CAS_URL = ask('Pełny adres serwera CAS', ['https://cas.amu.edu.pl/cas/'])
+        CAS_VERSION = ask('Wersja systemu CAS', [2, 1, 3])
 
+        APP_URL = ask('Adres, na którym hostowana jest aplikacja', ['https://ankieter.projektstudencki.pl'])
+        if APP_URL.endswith('/'):
+            APP_URL = APP_URL[:-1]
+
+        APP_PORT = ask('Port, na którym hostowana jest apliakcja', [443])
+        SSL_CERT = ask('Ścieżka do certyfikatu *.pem', ['/etc/letsencrypt/live/ankieter.projektstudencki.pl/fullchain.pem'])
+        SSL_PVKY = ask('Ścieżka do klucza prywantego *.pem', ['/etc/letsencrypt/live/ankieter.projektstudencki.pl/privkey.pem'])
+
+        DEBUG = 't' == ask('Czy aplikacja ma być uruchamiana w trybie testowym?', ['t', 'n'])
+
+        with open('config.py', 'w+') as cfg:
+            print(f"CAS_URL='{CAS_URL}'", file=cfg)
+            print(f"CAS_VERSION={CAS_VERSION}", file=cfg)
+            print(f"APP_URL='{APP_URL}'", file=cfg)
+            print(f"APP_PORT={APP_PORT}", file=cfg)
+            print(f"SSL_CONTEXT=('{SSL_CERT}', '{SSL_PVKY}')", file=cfg)
+            print(f"DEBUG={DEBUG}", file=cfg)
+        print("Zapisano konfigurację. Można ją później zmienić w pliku config.py")
+
+    if not os.path.exists('master.db') or 't' == ask('Baza danych użytkowników już istnieje, czy zamierzasz utworzyć ją na nowo?', ['n', 't']):
+        admin = ask('Nazwa użytkownika CAS, który będzie administratorem', ['admin'])
+        pesel = ask('PESEL administratora (umożliwi logowanie na dwa sposoby)', [99999999999])
+        setup(admin=admin, pesel=pesel)
